76aef9136fc33b9e0471566cde13052e37115d63
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-15 21:01:30 -0700

    ScoreKeeping
    goal script added
    points adding added
    scorekeeping added

---
+++ SoccerGame/Assets/Scripts/Goal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+
+    public string Team; //used to set ownership of the goal to determine who gets points
+    public delegate void GoalScored(string teamName, int points);
+    public static GoalScored OnGoalScored;
+
+    void OnTriggerEnter(Collider other) //when the ball enters the goal
+    {
+        OnGoalScored(Team, Statics.goalPointValue); //call event to add points, int is points to be added, team is who to add points to
+    }
+
+}
---
+++ SoccerGame/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour {
+
+    protected int RedScore, BlueScore;
+
+    private void OnEnable(){
+        Goal.OnGoalScored += AddPoints;
+    }
+
+    private void AddPoints(string team, int points) {
+        switch (team) {
+            case "Red":
+                break;
+            case "Blue":
+                break;
+        }
+    }
+
+}

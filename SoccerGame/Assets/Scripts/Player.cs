19a4392a7f7cdee8b6ee59000a4819a67a765baa
Fritz Dittmer <fdittmer9762@gmail.com>
2016-12-15 20:40:09 -0700

    PlayerInput
    added input manager script (for controller inputs)
    added player script (for player movement)
    added static scripts (for static and possably abstract vars or methods)

---
+++ SoccerGame/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public CharacterController playerCC;//contains the player transform
+    Vector3 tempPos;
+
+    // EVENT SUBS
+    private void OnEnable(){
+        InputManagaer.PlayerInput += PlayerMovement;
+    }
+
+    private void OnDisable()
+    {
+        InputManagaer.PlayerInput -= PlayerMovement;
+    }
+
+    void PlayerMovement(float inputH, float inputV) {
+        Debug.Log(inputH + inputV);
+        tempPos.x = inputH * Statics.playerSpeed;
+        tempPos.z = inputV * Statics.playerSpeed;
+        playerCC.Move(tempPos);
+    }
+
+}
